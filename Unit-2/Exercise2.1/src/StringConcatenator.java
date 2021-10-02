d9114d2dba144f972ef0d6dc59feb4d7e360ddb9
AJO2DAM-2122 <jamet21118@iesmarenostrum.com>
2021-10-02 19:55:09 +0200

    Unit 2 - Exercise 2.1
    Exercise 2.1 of Unit 2 completed - AJO2DAMU

---
+++ Unit-2/Exercise2.1/src/StringConcatenator.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class StringConcatenator {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        System.out.print("Enter your first name: ");
+        String name = myScanner.nextLine();
+        System.out.print("Enter your surnames: ");
+        String surnames = myScanner.nextLine();
+
+        System.out.println("Hello, " + name + " " + surnames + "!");
+        myScanner.close();
+    }
+}
