b5e1e919b1692b9af5fc0ddcabad22b025c6aa0f
AJO2DAM-2122 <jamet21118@iesmarenostrum.com>
2021-11-18 12:58:03 +0100

    Final Activity, first commit - AJO2122
    Graphic interface completed

---
+++ Unit-3/FinalActivity-AJO2122/src/main/java/u3/aajaor2122/com/App.java
@@ -0,0 +1,11 @@
+package u3.aajaor2122.com;
+
+import java.sql.*;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Unit-3/FinalActivity-AJO2122/src/main/java/u3/aajaor2122/com/MainForm.java
@@ -0,0 +1,30 @@
+package u3.aajaor2122.com;
+
+import javax.swing.*;
+
+public class MainForm {
+    private JPanel mainFormPanel;
+    private JTabbedPane Enrollment;
+    private JPanel Students;
+    private JPanel Enrollments;
+    private JLabel labelFirstName;
+    private JTextField textFirstName;
+    private JLabel labelLastName;
+    private JTextField textIDCard;
+    private JTextField textLastName;
+    private JTextField textEmail;
+    private JTextField textPhone;
+    private JLabel labelIDCard;
+    private JLabel labelEmail;
+    private JLabel labelPhone;
+    private JButton buttonAdd;
+    private JComboBox comboBox1;
+    private JComboBox comboBox2;
+    private JButton buttonEnroll;
+    private JComboBox cbStudentReports;
+    private JTextArea textScoresResult;
+    private JButton buttonPrint;
+    private JButton buttonImportXML;
+
+
+}
