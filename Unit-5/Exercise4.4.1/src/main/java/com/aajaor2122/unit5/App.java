3dfbe6d215fb68b20ea1e3256c3d9617cfdbcda4
AJO2DAM-2122 <jamet21118@iesmarenostrum.com>
2022-01-25 14:20:30 +0100

    Exercise 4.4.1 of Unit 5 - Done
    Exercise complete

--- Unit-5/Exercise4.4.1/src/main/java/com/aajaor2122/unit5/App.java
+++ Unit-5/Exercise4.4.1/src/main/java/com/aajaor2122/unit5/App.java
@@ -81,6 +81,7 @@ public class App
                         System.out.print("Enter the new location for the department: ");
                         String newDeptLoc = sc.nextLine();
                         updateDepartment(deptNum, newDeptName, newDeptLoc);
+                        break;
                     }
 
                     if (updateOption == 2) {
@@ -94,7 +95,53 @@ public class App
                         int deptNum = Integer.parseInt(sc.nextLine());
 
                         updateEmployee(empNum, newEmpName, newJob, deptNum);
+                        break;
                     }
+
+                // Option to delete existing entries in the database
+                case 4:
+                    int deleteOption = 0;
+                    System.out.println("1. Delete an existing department.");
+                    System.out.println("2. Delete an existing employee.");
+                    System.out.print("Choose one option: ");
+                    deleteOption = Integer.parseInt(sc.nextLine());
+
+                    if (deleteOption == 1) {
+                        System.out.print("Enter the number of the department you want to delete: ");
+                        int deptNum = Integer.parseInt(sc.nextLine());
+                        printDepartment(deptNum);
+                        System.out.print("Are you sure you want to delete this department? (Y/N): ");
+                        String yesOrNo = sc.nextLine();
+
+                        if (yesOrNo.equalsIgnoreCase("y")) {
+                            deleteDepartment(deptNum);
+                        } else {
+                            System.out.println("Delete operation canceled.\n");
+                        }
+
+                    } else if (deleteOption == 2) {
+                        System.out.print("Enter the number of the employee you want to delete: ");
+                        int empNum = Integer.parseInt(sc.nextLine());
+                        printEmployee(empNum);
+                        System.out.print("Are you sure you want to delete this employee? (Y/N): ");
+                        String yesOrNo = sc.nextLine();
+
+                        if (yesOrNo.equalsIgnoreCase("y")) {
+                            deleteEmployee(empNum);
+                        } else {
+                            System.out.println("Delete operation canceled.\n");
+                        }
+                    } else {
+                        System.out.println("Option is NOT a valid one.");
+                    }
+
+                    break;
+
+                case 0:
+                    System.exit(0);
+
+                default:
+                    System.out.println("Option is NOT correct. Please, enter some valid number.");
             }
         }
 
@@ -145,6 +192,40 @@ public class App
         }
     }
 
+    // Method to print the information of a single department
+    public static void printDepartment(int deptNum) {
+
+        try ( Session session = openSession() ) {
+            Query<DeptEntity> deptQuery =
+                    session.createQuery("from com.aajaor2122.unit5.DeptEntity where deptno='" +
+                            String.valueOf(deptNum) + "' ");
+            List<DeptEntity> departments = deptQuery.list();
+            DeptEntity department = departments.get(0);
+
+            System.out.printf("DEPARTMENT - Number: %d  Name: %s  Location: %s%n",
+                    department.getDeptno(), department.getDname(), department.getLoc());
+        }
+        catch (HibernateException hiex) {
+            System.err.println( hiex.getMessage() );
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void printEmployee(int empNum) {
+        try (Session session = openSession()) {
+            Query<EmployeeEntity> myQuery =
+                    session.createQuery("from com.aajaor2122.unit5.EmployeeEntity");
+            List<EmployeeEntity> employees = myQuery.list();
+            EmployeeEntity employee = employees.get(0);
+
+            System.out.printf("EMPLOYEE - Number: %d  Name: %s  Job: %s  Department: %s%n",
+                    employee.getEmpno(), employee.getEname(), employee.getJob(), printDeptToString(employee.getDepartment()));
+        }
+    }
+
     // Method to print the employee´s list in a String format
     public static String printEmployeesList(Set employeesList) {
 
@@ -157,6 +238,14 @@ public class App
         }
 
         return employeeData;
+    }
+
+    // Prints and converts department´s property of Employee into a String format
+    public static String printDeptToString(DeptEntity department) {
+
+        String departName = "";
+        departName = department.getDname();
+        return departName;
     }
 
     // Method to print all the data from Employee entities
@@ -197,6 +286,8 @@ public class App
             department.setLoc(dlocation);
             session.save(department);
             transaction.commit();
+
+            System.out.println("The department has been inserted into the database.\n");
         }
         catch (HibernateException hiex) {
             System.err.println( hiex.getMessage() );
@@ -221,6 +312,8 @@ public class App
             employee.setJob(ejob);
             session.save(employee);
             transaction.commit();
+
+            System.out.println("The employee has been inserted into the database.\n");
         }
         catch (HibernateException hiex) {
             System.err.println( hiex.getMessage() );
@@ -243,6 +336,7 @@ public class App
             session.update(department);
             transaction.commit();
 
+            System.out.println("The department has been updated./n");
         } catch (HibernateException hiex) {
             System.err.println( hiex.getMessage() );
         }
@@ -272,6 +366,7 @@ public class App
             session.update(employee);
             transaction.commit();
 
+            System.out.println("The employee has been updated./n");
         } catch (HibernateException hiex) {
             System.err.println( hiex.getMessage() );
         }
@@ -279,4 +374,55 @@ public class App
             e.printStackTrace();
         }
     }
+
+    public static void deleteDepartment(int deptNum) {
+        try (Session session = openSession()) {
+            Query<DeptEntity> deptQuery =
+                    session.createQuery("from com.aajaor2122.unit5.DeptEntity where deptno='" +
+                            String.valueOf(deptNum) + "' ");
+            List<DeptEntity> departments = deptQuery.list();
+            DeptEntity deletedDept = departments.get(0);
+            if (departments.size() > 0) {
+                Transaction transaction = session.beginTransaction();
+                // CODIGO ANTES DE HACER EL CAMBIO - VOLVER A PONER SI FALLA
+                //DeptEntity deletedDept = departments.get(0);
+                //session.delete(departments.get(0));
+                session.delete(deletedDept);
+                transaction.commit();
+                System.out.println("The department has been deleted.\n");
+            } else
+                System.out.println("The department has NOT been found.\n");
+
+        } catch (HibernateException hiex) {
+            System.err.println( hiex.getMessage() );
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void deleteEmployee(int empNum) {
+        try (Session session = openSession()) {
+            // We obtain the employee instance to be deleted
+            Query<EmployeeEntity> employeeQuery =
+                    session.createQuery("from com.aajaor2122.unit5.EmployeeEntity where empno='" +
+                            String.valueOf(empNum) + "' ");
+            List<EmployeeEntity> employees = employeeQuery.list();
+            EmployeeEntity deletedEmp = employees.get(0);
+
+            if (employees.size() > 0) {
+                Transaction transaction = session.beginTransaction();
+                session.delete(deletedEmp);
+                transaction.commit();
+                System.out.println("The employee has been deleted succesfully.\n");
+            } else
+                System.out.println("The employee has NOT been found.\n");
+        } catch (HibernateException hiex) {
+            System.err.println( hiex.getMessage() );
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
